8e3fa0b44da12b788be7028bc62b4a62388ead9c
Luan P Coelho <luan@byteslivres.com.br>
2021-07-07 22:36:32 -0300

    Create simple use case [Alfha 0.1]

---
+++ codebank/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"codebank/domain"
+	"codebank/infrastructure/repository"
+	"codebank/usecase"
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	db := setupDb()
+	defer db.Close()
+
+	cc := domain.NewCreditCard()
+	cc.Number = "1234123412341234"
+	cc.Name = "Luan"
+	cc.ExpirationYear = 2021
+	cc.ExpirationMonth = 7
+	cc.CVV = 123
+	cc.Limit = 1000
+	cc.Balance = 0
+
+	repo := repository.NewTransactionRepositoryDb(db)
+	err := repo.CreateCreditCard(*cc)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+func setupTransactionUseCase(db *sql.DB) usecase.UseCaseTransaction {
+	transactionRepository := repository.NewTransactionRepositoryDb(db)
+	useCase := usecase.NewUseCaseTransaction(transactionRepository)
+
+	return useCase
+}
+
+func setupDb() *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		"db",
+		"5432",
+		"postgres",
+		"root",
+		"codebank",
+	)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal("error connection to database")
+	}
+
+	return db
+}
